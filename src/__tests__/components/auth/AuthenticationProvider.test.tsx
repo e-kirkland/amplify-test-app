929a9de766edf997bde221b863f27cd1613e56b0
e-kirkland <wekrklnd@gmail.com>
2025-08-01 05:55:30 -0400

    refactor: Remove unnecessary React import from test files
    - Removed the unused import of React in AuthenticationProvider.test.tsx and RegistrationForm.test.tsx to clean up the code and adhere to best practices.

--- src/__tests__/components/auth/AuthenticationProvider.test.tsx
+++ src/__tests__/components/auth/AuthenticationProvider.test.tsx
@@ -1,6 +1,5 @@
 import { AuthenticationProvider } from '../../../components/auth/AuthenticationProvider';
 
-import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
